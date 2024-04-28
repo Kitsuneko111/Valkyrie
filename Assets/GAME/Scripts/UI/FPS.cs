cab01f3403a0d699e3f8f7d6b80fa683d900e890
Kitsuneko <53912018+Kitsuneko111@users.noreply.github.com>
2024-04-28 01:25:48 +0100

    Created dev view scripts to generate values automatically

---
+++ Assets/GAME/Scripts/UI/FPS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using static Unity.Burst.Intrinsics.X86;
+using System.Linq;
+
+namespace Project.UI{
+    
+    public class FPS : MonoBehaviour
+    {
+        TMP_Text text;
+        Queue<(double, float)> historic = new Queue<(double, float)>();
+
+        private void Start()
+        {
+            text = GetComponent<TMP_Text>();
+        }
+        void Update()
+        {
+            double now = Time.realtimeSinceStartup;
+            historic.Enqueue((now, Time.deltaTime));
+            while (historic.Peek().Item1 < now-10) historic.Dequeue();
+            float averageFPS = historic.Average(x => x.Item2);
+            text.text = $"FPS: {(int)(1/averageFPS)} (avg/10s)";
+        }
+    }
+    
+}
---
+++ Assets/GAME/Scripts/UI/FSpeed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Project.Character;
+using TMPro;
+
+namespace Project.UI{
+    
+    public class FSpeed : MonoBehaviour
+    {
+        Character.CharacterController character;
+        TMP_Text text;
+        // Start is called before the first frame update
+        void Start()
+        {
+            character = FindObjectOfType<Character.CharacterController>();
+            text = GetComponent<TMP_Text>();
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            text.text = $"FSpeed: {Mathf.Round(new Vector2(character.rb.velocity.x, character.rb.velocity.z).magnitude*100)/100}m/s";
+        }
+    }
+    
+}
